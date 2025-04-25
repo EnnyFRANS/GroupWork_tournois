092f08b20659538ae8a61ea9c4069146c0d05fbf
Mélanie V. <mel.vdrst89@gmail.com>
2025-04-25 14:20:51 +0200

    point losing or winning :level/health

--- GroupProjectFirst/app.py
+++ GroupProjectFirst/app.py
@@ -3,10 +3,10 @@ from Player import *
 import random
 
 app = Flask(__name__)
-
+player = Player(1,10)
 @app.route('/')
 def start():
-    player = Player(1,10)
+
     return render_template('start.html', player=player)
 
 @app.route('/battle/<string:enemy>')
@@ -14,13 +14,25 @@ def battle(enemy):
     rnd = random.choice((-1, 1))
     return render_template('battle.html', enemy=enemy, rnd=rnd)
 
-@app.route('/win')
-def win():
-    return render_template('win.html')
+@app.route('/win/<string:enemy>')
+def win(enemy):
+    if enemy == "Pikachu":
+        player.level += 1
+    elif enemy == "Wyverne":
+        player.level += 5
+    else:
+        player.level +=3
+    return render_template('win.html', player=player)
 
-@app.route('/lose')
-def lose():
-    return render_template('lose.html')
+@app.route('/lose/<string:enemy>')
+def lose(enemy):
+    if enemy == "Pikachu":
+        player.health -= 1
+    elif enemy == "Wyverne":
+        player.health -= 5
+    else:
+        player.health -= 3
+    return render_template('lose.html', player=player)
 
 @app.route('/game_over')
 def game_over():
