d7bcbd2b4d09d300b276862af8416bb0e91515f5
Tatiana S <tatyana.skvortsova@gmail.com>
2025-04-25 16:23:56 +0200

    sounds

--- GroupProjectFirst/app.py
+++ GroupProjectFirst/app.py
@@ -1,9 +1,18 @@
 from flask import Flask, render_template
 from Player import *
 import random
+from pygame import mixer
+
+
+def playsound(path):
+    mixer.init()
+    mixer.music.load(path)
+    mixer.music.play()
 
 app = Flask(__name__)
+
 player = Player(1,10)
+
 @app.route('/')
 def start():
 
@@ -12,10 +21,17 @@ def start():
 @app.route('/battle/<string:enemy>')
 def battle(enemy):
     rnd = random.choice((-1, 1))
+    if enemy == "Pikachu":
+        playsound("./sound/700348__silasonlyw__pikachu-pokemon.wav")
+    elif enemy == "Wyvern":
+        playsound("./sound/4505__noisecollector__dragon2.wav")
+    else:
+        playsound("./sound/798331__dr_dig1__troll-or-monster-laughing.mp3")
     return render_template('battle.html', enemy=enemy, rnd=rnd)
 
 @app.route('/win/<string:enemy>')
 def win(enemy):
+    playsound("./sound/270319__littlerobotsoundfactory__jingle_win_01.wav")
     if enemy == "Pikachu":
         player.level += 1
     elif enemy == "Wyvern":
@@ -32,6 +48,11 @@ def lose(enemy):
         player.health -= 5
     else:
         player.health -= 3
+    if player.health <= 0:
+        playsound("./sound/524741__lilmati__game-over-08.wav")
+    else:
+        playsound("./sound/350981__cabled_mess__lose_c_09.wav")
+
     return render_template('lose.html', player=player)
 
 if __name__ == '__main__':
